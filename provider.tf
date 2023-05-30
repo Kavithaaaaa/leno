0b68ffcc0127b0882165ed6985d4eaae5b1e9a27
Ubuntu <ubuntu@ip-172-31-1-155.us-east-2.compute.internal>
2023-05-30 16:34:16 +0000

    16

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = "~> 1.4.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.50.0"
+      source  = "hashicorp/aws"
+      version = "~>3.50.0"
     }
   }
 }
 # Provider-1 for us-east-1 (Default Provider)
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "default"
 }
